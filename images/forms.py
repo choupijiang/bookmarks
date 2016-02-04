8888a8cbb25ad45d6678e73a2bce0e0a6e655416
choupijiang <zhangshenghu@gmail.com>
2016-02-04 14:24:31 +0800

    'add'

---
+++ images/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Image
+import urllib2
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given url does not match valid image extensions')
+        return url
+
+    def save(self, commit=True,  force_insert = False, force_update= False):
+        image = super(ImageCreateForm, self).save(commit = False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+        response = urllib2.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+        if commit:
+            image.save()
+        return image
